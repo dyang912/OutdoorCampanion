e0a6f56747596ac051bbc3196939d1f34f276e62
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-01-15 16:11:01 -0600

    sorting functionality

--- src/database/fetchposts.js
+++ src/database/fetchposts.js
@@ -1,15 +1,16 @@
-import { getDatabase, ref, child,get, orderByChild } from "firebase/database";
+import { getDatabase, ref, child,get, orderByChild, limitToLast } from "firebase/database";
 
 const db = getDatabase();
 
 export async function fetch_posts() {
-    return await get(child(ref(db), `posts`, orderByChild('time'))).then((snapshot) => {
+    return await get(child(ref(db), `posts`, orderByChild('time'), limitToLast(3))).then((snapshot) => {
         const postarray = [];
         snapshot.forEach(function(child) {
-            console.log(child.val());
             postarray.push(child.val());
         });
+        console.log(postarray);
         if (snapshot.exists()) {
+            postarray.sort((a, b) => b.time - a.time);
             return postarray;
         }
     }).catch((error) => {
