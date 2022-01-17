9e7e678e5cacb55b2f11f8e4be001e4c148d4b73
Dong Yang <outskidding@gmail.com>
2022-01-16 22:53:41 -0600

    solve warning

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -12,22 +12,22 @@ function NavigationBar() {
                 <li className = "nav-item mx-5">
                     <Link to={"/"} className= "nav-link">
                         { location.pathname === "/" ?
-                            <i className="bi bi-house-fill" style={{"font-size":"25px"}}/> :
-                            <i className="bi bi-house" style={{"font-size":"25px"}}/> }
+                            <i className="bi bi-house-fill" style={{fontSize:"25px"}}/> :
+                            <i className="bi bi-house" style={{fontSize:"25px"}}/> }
                     </Link>
                 </li>
                 <li className = "nav-item mx-5">
                     <Link to={"/newpost"} className= "nav-link">
                         { location.pathname === "/newpost" ?
-                            <i className="bi bi-plus-square-fill" style={{"font-size":"25px"}}/> :
-                            <i className="bi bi-plus-square" style={{"font-size":"25px"}}/> }
+                            <i className="bi bi-plus-square-fill" style={{fontSize:"25px"}}/> :
+                            <i className="bi bi-plus-square" style={{fontSize:"25px"}}/> }
                     </Link>
                 </li>
                 <li className = "nav-item mx-5">
                     <Link to={"/login"} className= "nav-link">
                         { location.pathname === "/login" ?
-                            <i className="bi bi-person-fill" style={{"font-size":"25px"}}/> :
-                            <i className="bi bi-person" style={{"font-size":"25px"}}/> }
+                            <i className="bi bi-person-fill" style={{fontSize:"25px"}}/> :
+                            <i className="bi bi-person" style={{fontSize:"25px"}}/> }
                     </Link>
                 </li>
             </ul>
