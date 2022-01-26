0cdb3f7db27aaeaf086f21ae7e97aee243b42640
Liqian Ma <liqianma2023@u.northwestern.edu>
2022-01-25 18:28:13 -0600

    change comment open and close

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import {Card, Image} from "react-bootstrap";
 import {Comments} from "./comments";
 import {isMobile} from 'react-device-detect';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 export const Feed = ({ posts, category }) => {
     return (
@@ -15,25 +17,21 @@ export const Feed = ({ posts, category }) => {
 
 const Post = ({ post, handleClick }) => {
     const [selected, setSelected] = useState(false);
-
+    const [commentOpen, setOpen] = useState(false);
     return (
         <Card className="m-2"
-              onClick={() => setSelected(true)}
-              onDoubleClick={() => setSelected(false)}
+            //   onClick={() => setSelected(true)}
+            //   onDoubleClick={() => setSelected(false)}
         >
             <Card.Body>
                 <Card.Title>{ post.text }</Card.Title>
-                { post.image?
-                    isMobile ?
-                        <Image src={post.image} alt="postImage" fluid={true} style={{maxWidth:"100%"}}/> :
-                        <Image src={post.image} alt="postImage" fluid={true} style={{maxWidth:"30%"}}/>
-                    : null
-                }
-                <Card.Text>
-                    <small className="text-muted">{ post.creator + " " + new Date(post.time).toLocaleString() }</small>
-                </Card.Text>
+                { post.image? <img src = { post.image } alt="postImage"/> : null }
+                <Card.Text>{post.creator + " " + new Date(post.time).toLocaleString()}</Card.Text>
+                {selected ? <button type="button" className="btn btn-secondary" onClick={() => setSelected(false)}>close comments</button> : <button type="button" className="btn btn-outline-dark" onClick={() => setSelected(true)}>{!post.comments ? 0 : Object.keys(post.comments).length} comment(s)</button>}
             </Card.Body>
             {selected ? <Comments postKey={post.postKey}/>: null}
         </Card>
     );
+
+    // <i class="bi bi-x"></i>
 }
