59461b641989e34b289f74651fe44ef179168ba8
Dong Yang <outskidding@gmail.com>
2022-02-08 19:57:04 -0600

    remove the scrollbar on the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,15 +13,17 @@ function Login({ user, UEmail, setUEmail, UName, setUName, UPhotoUrl, setUPhotoU
             { user ? null : <p className = "login-header">Login to unlock the outdoors!</p> }
             { user ? <SignOutButton setUEmail={setUEmail} setUName={setUName} setUPhotoUrl = {setUPhotoUrl} setUid={setUid}/> :
                       <SignInButton setUEmail={setUEmail} setUName={setUName} setUPhotoUrl = {setUPhotoUrl}/> }
-            <div className="row">
-                <div className="col" id ="profile-photo-container">
-                    {user?  <img className = "profile-photo" src={UPhotoUrl} alt="UserPhoto"/> : null}
-                </div>
-                <div className="col">
-                    {user? <div>Your Name: {UName ? UName : "NULL"}</div> : null}
-                    {user? <div>Your email: {UEmail ? UEmail : "NULL"}</div> : null}
-                    {user? <div> Total likes: {totalLikes} </div> :null}
-                    
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6" id ="profile-photo-container">
+                        {user?  <img className = "profile-photo" src={UPhotoUrl} alt="UserPhoto"/> : null}
+                    </div>
+                    <div className="col-sm-6">
+                        {user? <div>Your Name: {UName ? UName : "NULL"}</div> : null}
+                        {user? <div>Your email: {UEmail ? UEmail : "NULL"}</div> : null}
+                        {user? <div> Total likes: {totalLikes} </div> :null}
+
+                    </div>
                 </div>
             </div>
         </div>
